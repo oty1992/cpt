be8c0e83c51382c8bba24bbc0bf694366b347c18
oty1992 <oty1992@gmail.com>
2023-04-07 15:37:50 +0900

    feat(server): Add room middleware

---
+++ server/src/middleware/room.ts
@@ -0,0 +1,50 @@
+import type { NextFunction, OpineRequest, OpineResponse } from 'opine';
+import { roomRepository } from '~/model/room.ts';
+import { throwError } from '~/middleware/error_handler.ts';
+
+const AUTH_ERROR = {
+  status: 401,
+  message: 'Authorization Error',
+};
+
+export const isInRoom = async (
+  req: OpineRequest,
+  _res: OpineResponse,
+  next: NextFunction,
+) => {
+  const { method, originalUrl } = req;
+  const roomId = req.params.id;
+  const userId = req.body.userId;
+
+  try {
+    if (!roomId) {
+      return throwError({
+        method,
+        baseUrl: originalUrl,
+        ...AUTH_ERROR,
+      });
+    }
+    const room = await roomRepository.findById(roomId);
+    if (!room) {
+      return throwError({
+        method,
+        baseUrl: originalUrl,
+        ...AUTH_ERROR,
+      });
+    }
+    if (!room.users.map((users) => users.id).includes(userId)) {
+      return throwError({
+        method,
+        baseUrl: originalUrl,
+        ...AUTH_ERROR,
+      });
+    }
+    next();
+  } catch (_e) {
+    return throwError({
+      method,
+      baseUrl: originalUrl,
+      ...AUTH_ERROR,
+    });
+  }
+};
