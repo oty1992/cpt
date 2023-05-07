7f0a649a41954e1ca7f93b1ac81d58ebeac7a0d1
oty1992 <oty1992@gmail.com>
2023-05-07 17:36:17 +0900

    feat(client): Add react query

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,22 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Outlet } from 'react-router-dom';
 import AuthApi from './api/auth';
 import { Header } from './components/Header';
 import { AuthContextProvider } from './contexts/AuthContext';
 import HttpClient from './networks/http';
 
+const queryClient = new QueryClient();
+
 const authApi = new AuthApi(new HttpClient(import.meta.env.VITE_SERVER_URL));
 
 function App() {
   return (
-    <AuthContextProvider authApi={authApi}>
-      <Header />
-      <Outlet />
-    </AuthContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider authApi={authApi}>
+        <Header />
+        <Outlet />
+      </AuthContextProvider>
+    </QueryClientProvider>
   );
 }
 
