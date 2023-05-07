ff3132868ce9131ac6010a07272e7f6cc5ee4e3a
oty1992 <oty1992@gmail.com>
2023-05-07 17:21:12 +0900

    feat(client): Add AuthContext

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,17 @@
 import { Outlet } from 'react-router-dom';
+import AuthApi from './api/auth';
 import { Header } from './components/Header';
+import { AuthContextProvider } from './contexts/AuthContext';
+import HttpClient from './networks/http';
+
+const authApi = new AuthApi(new HttpClient(import.meta.env.VITE_SERVER_URL));
 
 function App() {
   return (
-    <>
+    <AuthContextProvider authApi={authApi}>
       <Header />
       <Outlet />
-    </>
+    </AuthContextProvider>
   );
 }
 
---
+++ client/src/contexts/AuthContext.tsx
@@ -0,0 +1,80 @@
+import type { PropsWithChildren } from 'react';
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
+import type {
+  AuthToken,
+  IAuthApi,
+  LoginInfo,
+  SignUpInfo,
+  UserInfo,
+} from '../types';
+
+type AuthContextType = {
+  user: AuthToken | null;
+  signup(user: SignUpInfo): Promise<void>;
+  login(loginInfo: LoginInfo): Promise<void>;
+  logout(): Promise<void>;
+};
+
+const AuthContext = createContext<AuthContextType>({
+  user: null,
+  signup: async () => {},
+  login: async () => {},
+  logout: async () => {},
+});
+
+type AuthContextProps = PropsWithChildren & {
+  authApi: IAuthApi;
+};
+
+export function AuthContextProvider({ authApi, children }: AuthContextProps) {
+  const [user, setUser] = useState<AuthToken | null>(null);
+
+  const handleToken = (token: AuthToken | null) => {
+    if (token?.username !== user?.username) setUser(token);
+  };
+
+  useEffect(() => {
+    authApi
+      .me()
+      .then((token) => {
+        if (token) {
+          handleToken(token);
+        }
+      })
+      .catch(() => handleToken(null));
+  }, [user]);
+
+  const signup = useCallback(async (user: UserInfo) => {
+    const token = await authApi.signup(user);
+    setUser(token);
+  }, []);
+
+  const login = useCallback(async (loginInfo: LoginInfo) => {
+    const token = await authApi.login(loginInfo);
+    setUser(token);
+  }, []);
+
+  const logout = useCallback(async () => {
+    await authApi.logout();
+    setUser(null);
+  }, []);
+
+  const context = useMemo(
+    () => ({ user, signup, login, logout }),
+    [user, signup, login, logout],
+  );
+
+  return <AuthContext.Provider value={context}>{children}
+  </AuthContext.Provider>;
+}
+
+export function useAuthContext() {
+  return useContext(AuthContext);
+}
