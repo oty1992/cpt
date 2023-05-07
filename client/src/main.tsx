c330fa0b1bf626996c29b22e93a6125d5562807c
oty1992 <oty1992@gmail.com>
2023-05-07 17:23:14 +0900

    feat(client): Add SignIn page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 import NotFound from './pages/NotFound';
+import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import './index.css';
 
@@ -11,12 +12,15 @@ const router = createBrowserRouter([
     path: '/',
     element: <App />,
     errorElement: <NotFound />,
-    children: [{ path: 'signup', element: <SignUp /> }],
+    children: [
+      { path: 'signin', element: <SignIn /> },
+      { path: 'signup', element: <SignUp /> },
+    ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
---
+++ client/src/pages/SignIn.tsx
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import type { LoginInfo } from '../types';
+import { useAuthContext } from '../contexts/AuthContext';
+import { validateUser } from '../utils/validator';
+
+type LoginInfoValidation = {
+  username: boolean;
+  password: boolean;
+};
+
+const initialLoginInfo: LoginInfo = {
+  username: '',
+  password: '',
+};
+
+export default function SignIn() {
+  const { user, login } = useAuthContext();
+
+  const [loginInfo, setLoginInfo] = useState<LoginInfo>(initialLoginInfo);
+  const [validation, setValidation] = useState<LoginInfoValidation>({
+    username: false,
+    password: false,
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setValidation((validation) => ({
+      ...validation,
+      [name]: validateUser(name, value),
+    }));
+    setLoginInfo((loginInfo) => ({ ...loginInfo, [name]: value }));
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!Object.values(validation).includes(false)) {
+      login(loginInfo);
+      navigate('/', { replace: true });
+    }
+  };
+
+  useEffect(() => {
+    !!user && navigate('/');
+  }, [user]);
+
+  return (
+    <section>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username</label>
+          <input
+            type='text'
+            name='username'
+            value={loginInfo.username ?? ''}
+            placeholder='username'
+            required
+            onChange={handleChange}
+          />
+          <label>Password</label>
+          <input
+            type='password'
+            name='password'
+            value={loginInfo.password ?? ''}
+            placeholder='password'
+            required
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <button type='submit'>Sign in</button>
+        </div>
+      </form>
+    </section>
+  );
+}
