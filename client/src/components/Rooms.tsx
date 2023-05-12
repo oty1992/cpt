01e156ab8fe0a4324562896c8f1ec01f02254f78
oty1992 <oty1992@gmail.com>
2023-05-12 16:20:36 +0900

    feat(client): Add create Action to Rooms component

--- client/src/components/Rooms.tsx
+++ client/src/components/Rooms.tsx
@@ -1,15 +1,32 @@
+import { useNavigate } from 'react-router-dom';
 import useRooms from '../hooks/useRooms';
 import RoomCard from './RoomCard';
+import Action from './ui/Action';
 
 export default function Rooms() {
   const { roomsQuery: { data: rooms } } = useRooms();
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/room/create');
+  };
+
   return (
     <section className='flex flex-col justify-center items-center w-full'>
       <h1 className='text-5xl text-slate-800 font-bold py-6'>Rooms</h1>
-      <ul className='flex flex-col items-center gap-2 w-2/3 p-10 rounded-3xl bg-slate-200'>
-        {rooms && rooms.map((room) => <RoomCard key={room.id} room={room} />)}
-      </ul>
-      {/* TODO: Add Create Room */}
+      <div className='relative w-2/3 p-10 rounded-3xl bg-slate-200'>
+        <ul className='flex flex-col gap-2 w-full mb-4'>
+          {rooms && rooms.map((room) => <RoomCard key={room.id} room={room} />)}
+        </ul>
+        <div className='absolute bottom-2 right-3'>
+          <Action
+            actionType='button'
+            title='Create'
+            isDisable={false}
+            onClick={handleClick}
+          />
+        </div>
+      </div>
     </section>
   );
 }
