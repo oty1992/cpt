dad6211d8818048151c217b8fdf943e25a7f4ff5
oty1992 <oty1992@gmail.com>
2023-05-12 16:19:38 +0900

    feat(client): Add style to Rooms components

--- client/src/components/Rooms.tsx
+++ client/src/components/Rooms.tsx
@@ -4,12 +4,12 @@ import RoomCard from './RoomCard';
 export default function Rooms() {
   const { roomsQuery: { data: rooms } } = useRooms();
   return (
-    <>
-      <h1>Rooms</h1>
-      <ul>
+    <section className='flex flex-col justify-center items-center w-full'>
+      <h1 className='text-5xl text-slate-800 font-bold py-6'>Rooms</h1>
+      <ul className='flex flex-col items-center gap-2 w-2/3 p-10 rounded-3xl bg-slate-200'>
         {rooms && rooms.map((room) => <RoomCard key={room.id} room={room} />)}
       </ul>
       {/* TODO: Add Create Room */}
-    </>
+    </section>
   );
 }
