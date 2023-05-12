f5419242a737a1f5d84e1323399931b4371abce6
oty1992 <oty1992@gmail.com>
2023-05-12 16:23:15 +0900

    feat(client): Add NoRoomCard component

---
+++ client/src/components/NoRoomCard.tsx
@@ -0,0 +1,7 @@
+export default function RoomCard() {
+  return (
+    <li className='flex flex-col items-center gap-1 w-full px-4 py-3 rounded-xl bg-slate-300'>
+      <div className='font-semibold text-3xl text-slate-500 py-2'>No Room</div>
+    </li>
+  );
+}
--- client/src/components/Rooms.tsx
+++ client/src/components/Rooms.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import useRooms from '../hooks/useRooms';
+import NoRoomCard from './NoRoomCard';
 import RoomCard from './RoomCard';
 import Action from './ui/Action';
 
@@ -16,7 +17,9 @@ export default function Rooms() {
       <h1 className='text-5xl text-slate-800 font-bold py-6'>Rooms</h1>
       <div className='relative w-2/3 p-10 rounded-3xl bg-slate-200'>
         <ul className='flex flex-col gap-2 w-full mb-4'>
-          {rooms && rooms.map((room) => <RoomCard key={room.id} room={room} />)}
+          {!rooms?.length
+            ? <NoRoomCard />
+            : rooms.map((room) => <RoomCard key={room.id} room={room} />)}
         </ul>
         <div className='absolute bottom-2 right-3'>
           <Action
