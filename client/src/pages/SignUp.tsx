36de37dcf205ec6eed4dbd8ccd4027ce461959f4
oty1992 <oty1992@gmail.com>
2023-05-12 15:32:36 +0900

    feat(client): Add style to SignUp page

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -48,42 +48,85 @@ export default function SignUp() {
   }, [user]);
 
   return (
-    <section>
-      <h1>Sign Up</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Username</label>
-          <input
-            type='text'
-            name='username'
-            value={userInfo.username ?? ''}
-            placeholder='username'
-            required
-            onChange={handleChange}
-          />
-          <label>Password</label>
-          <input
-            type='password'
-            name='password'
-            value={userInfo.password ?? ''}
-            placeholder='password'
-            required
-            onChange={handleChange}
-          />
-          <label>Email</label>
-          <input
-            type='email'
-            name='email'
-            value={userInfo.email ?? ''}
-            placeholder='email'
-            required
-            pattern='[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$'
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <button type='submit'>Sign Up</button>
-        </div>
+    <section className='flex flex-col justify-center items-center w-full mt-16'>
+      <h1 className='text-5xl text-slate-800 font-bold py-6'>Sign Up</h1>
+      <form
+        className='relative flex justify-center w-80 px-6 py-10 rounded-3xl bg-slate-200'
+        onSubmit={handleSubmit}
+      >
+        <ul className='flex flex-col w-11/12 gap-1 mb-6'>
+          <li>
+            <label
+              className={`text-sm font-bold ${
+                validation.username ? 'text-slate-600' : 'text-red-500'
+              }`}
+            >
+              Username
+            </label>
+            <input
+              className={`w-full rounded-sm bg-inherit outline outline-2 ${
+                validation.username ? 'outline-slate-600' : 'outline-red-600'
+              }`}
+              type='text'
+              name='username'
+              value={userInfo.username ?? ''}
+              placeholder='username'
+              required
+              onChange={handleChange}
+            />
+          </li>
+          <li>
+            <label
+              className={`text-sm font-bold ${
+                validation.password ? 'text-slate-600' : 'text-red-500'
+              }`}
+            >
+              Password
+            </label>
+            <input
+              className={`w-full rounded-sm bg-inherit outline outline-2 ${
+                validation.password ? 'outline-slate-600' : 'outline-red-600'
+              }`}
+              type='password'
+              name='password'
+              value={userInfo.password ?? ''}
+              placeholder='password'
+              required
+              onChange={handleChange}
+            />
+          </li>
+          <li>
+            <label
+              className={`text-sm font-bold ${
+                validation.email ? 'text-slate-600' : 'text-red-500'
+              }`}
+            >
+              Email
+            </label>
+            <input
+              className={`w-full rounded-sm bg-inherit outline outline-2 ${
+                validation.email ? 'outline-slate-600' : 'outline-red-600'
+              }`}
+              type='email'
+              name='email'
+              value={userInfo.email ?? ''}
+              placeholder='email'
+              required
+              pattern='[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$'
+              onChange={handleChange}
+            />
+          </li>
+        </ul>
+        <button
+          className={`absolute font-medium bottom-2 right-3 rounded-3xl px-3 py-2 ${
+            Object.values(validation).includes(false)
+              ? 'text-slate-400 cursor-not-allowed'
+              : 'text-slate-600 hover:bg-slate-400'
+          }`}
+          type='submit'
+        >
+          Sign Up
+        </button>
       </form>
     </section>
   );
