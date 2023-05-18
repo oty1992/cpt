8b646ec4369a00584f992ba05baefc3185aef3f0
oty1992 <oty1992@gmail.com>
2023-05-18 16:06:24 +0900

    feat(client): Add content to the About component

--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -3,16 +3,21 @@ import Section from '~/components/ui/Section';
 
 export default function About() {
   return (
-    <Section className='w-full px-4 sm:px-10 md:px-32 lg:px-48 xl:px-60 2xl:px-80'>
+    <Section className='w-[80vw] max-w-3xl mx-auto mb-8'>
       <h1 className='text-5xl text-slate-800 font-bold py-6'>About</h1>
-      <ArticleCard className='mt-2 p-8 text-3xl text-slate-600'>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
+      <ArticleCard className='mt-2 px-8 py-6 sm:px-16 sm:py-12 text-slate-600'>
+        <h2 className='text-4xl text-justify font-bold text-slate-700'>
+          New message paradigm to prevent verbal violence on the Internet in
+          modern society, chat-gpt filtering
+        </h2>
+        <ArticleCard className='mt-12 p-4 sm:p-6 md:p-8 bg-slate-300'>
+          <ul className='flex flex-col text-3xl text-center gap-3'>
+            <li>Sophisticated filtering</li>
+            <li>Context understanding</li>
+            <li>Support for multiple languages</li>
+            <li>Real-time adaptation</li>
+          </ul>
+        </ArticleCard>
       </ArticleCard>
     </Section>
   );
