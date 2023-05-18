44e508960dce1adb54f420698e084a2dea533023
oty1992 <oty1992@gmail.com>
2023-05-18 15:37:53 +0900

    feat(client): Add style to User component

--- client/src/components/User.tsx
+++ client/src/components/User.tsx
@@ -6,8 +6,21 @@ type UserProps = {
 
 export default function User({ user }: UserProps) {
   return (
-    <>
-      <div>{user.username[0].toUpperCase()}</div>
-    </>
+    <div
+      className='w-10 h-10 inline-flex items-center justify-center rounded-full font-medium text-slate-700'
+      style={{ backgroundColor: stringToColor(user.userId) }}
+    >
+      {user.username[0].toUpperCase()}
+    </div>
   );
+}
+
+function stringToColor(str: string) {
+  let hash = str.split('').reduce((hash, char) => {
+    return char.charCodeAt(0) + ((hash << 5) - hash);
+  }, 0);
+
+  return [0, 1, 2].reduce((color, i) => {
+    return color + ('00' + ((hash >> (i * 8)) & 0xFF).toString(16)).slice(-2);
+  }, '#');
 }
