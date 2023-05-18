20c392aab34cbe637070c36572b08e0a5d9a13fb
oty1992 <oty1992@gmail.com>
2023-05-18 15:47:41 +0900

    feat(client): Add scroll to bottom in ChatList

--- client/src/components/ChatList.tsx
+++ client/src/components/ChatList.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import type { RoomInfo } from '~/types';
 import ArticleCard from '~/components/ui/ArticleCard';
 import ChatBubble from '~/components/ChatBubble';
@@ -7,11 +8,25 @@ type ChatListProps = {
   room: RoomInfo;
 };
 
-export default function ChatList({ userId, room }: ChatListProps) {
+export default function ChatList({ userId, room: { chats } }: ChatListProps) {
+  const ref = useRef<HTMLUListElement>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.scrollTo({
+        top: ref.current.scrollHeight,
+        behavior: 'smooth',
+      });
+    }
+  }, [chats]);
+
   return (
     <ArticleCard className='flex justify-center w-[90vw] sm:w-[36rem] h-[70vh]'>
-      <ul className='flex flex-col gap-2 p-6 overflow-y-auto scrollbar-none'>
-        {room.chats.map((chat) => (
+      <ul
+        ref={ref}
+        className='flex flex-col gap-2 p-6 overflow-y-auto scrollbar-none'
+      >
+        {chats.map((chat) => (
           <ChatBubble
             key={chat.created_at}
             chat={chat}
