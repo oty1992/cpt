a33c50b63185f97493c34479c26c1d0e978af9f2
oty1992 <oty1992@gmail.com>
2023-05-18 15:52:59 +0900

    feat(client): Add style to NotFound page

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,10 +1,15 @@
 import { Link } from 'react-router-dom';
+import { Header } from '~/components/Header';
+import Section from '~/components/ui/Section';
 
 export default function NotFound() {
   return (
-    <section>
-      <h1>Not Found</h1>
-      <Link to='/'>Go To Home</Link>
-    </section>
+    <>
+      <Header />
+      <Section>
+        <h1 className='text-5xl text-slate-800 font-bold py-6'>Not Found</h1>
+        <Link to='/'>Go To Home</Link>
+      </Section>
+    </>
   );
 }
